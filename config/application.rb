7d1d8ee3e78705cf19257a06b8516db5054d0202
Simon Hock <simon.hock@hotmail.com>
2014-11-30 20:28:34 +0100

    debug heroku

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+config.assets.initialize_on_precompile = false
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,6 +9,7 @@ Bundler.require(:default, Rails.env)
 
 module Rsnap
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
